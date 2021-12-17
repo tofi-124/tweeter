37db49e751c77a23e689aec0a9376062d7a75889
Tofik Mohammed <tofikahm@yahoo.com>
2021-12-17 00:47:06 +0000

    readme updated

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,7 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
 const $btnUp = $("#scrollUp");
 $btnUp.on("click", () =>
   window.scrollTo({
